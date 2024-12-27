f304d874c988e1d061113aa6a14f10272d062eb5
folieds <yosnkooke@gmail.com>
2024-12-27 15:33:36 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -172,7 +172,7 @@ PROGRESS = """
 
 🔥 Forwarded : {2}
 
-🫠 Remaining : {3}
+⚡ Remaining : {3}
 
 📊 Status : {4}
 
@@ -210,8 +210,8 @@ async def edit(msg, title, status, sts):
    text = TEXT.format(i.fetched, i.total_files, i.duplicate, i.deleted, i.skip, status, percentage, estimated_total_time, progress)
    if status in ["cancelled", "completed"]:
       button.append(
-         [InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-         InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')]
+         [InlineKeyboardButton('📢 Updates', url='https://t.me/outlawbots'),
+         InlineKeyboardButton('💬 Support', url='https://t.me/offchats')]
          )
    else:
       button.append([InlineKeyboardButton('✖️ Cancel ✖️', 'terminate_frwd')])
